8fddb07c0e7dad9e9a39d9f7d652e05495edd056
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-03-15 18:53:46 -0300

    concluido o header no mobile

---
+++ escript/escript.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('btn_menu')
+
+const nav = document.querySelector('.menu')
+
+const overlay = document.querySelector('.overlay')
+
+btn.addEventListener('click', ()=> {
+    nav.classList.toggle("menu_ativo")
+    overlay.classList.toggle("ativo")
+    
+})
+
+overlay.addEventListener("click", ()=>{
+    nav.classList.remove("menu_ativo")
+    overlay.classList.remove("ativo")
+})
+
