8bbab79ccb8e834760af5744a434e74a9bd2a928
DiogoIV <diogorodriguesdasilva156@gmail.com>
2026-05-02 14:53:25 -0300

    att

--- escript/escript.js
+++ escript/escript.js
@@ -1,19 +1,30 @@
+const main = document.querySelector('conteudo')
 const btn = document.getElementById('btn_menu')
-
 const nav = document.querySelector('.menu')
-
 const overlay = document.querySelector('.overlay')
 
-btn.addEventListener('click', ()=> {
-    nav.classList.toggle("menu_ativo")
-    overlay.classList.toggle("ativo")
-    
-})
 
-overlay.addEventListener("click", ()=>{
-    nav.classList.remove("menu_ativo")
-    overlay.classList.remove("ativo")
-})
+function menu () {
+    btn.addEventListener('click', ()=> {
+        nav.classList.toggle("menu_ativo")
+        overlay.classList.toggle("ativo")
+        
+    })
+    
+    overlay.addEventListener("click", ()=>{
+        nav.classList.remove("menu_ativo")
+        overlay.classList.remove("ativo")
+    })
+}
+
+async function carregarPagina (pagina) {
+    
+}
+
+
+
+
+menu()
 
 
 
